5a0db839a20d1ea6a2ca45c4d883d1f288240820
Nobal Dhruw <nobal.kishor@elucidata.io>
2021-06-17 09:29:41 +0530

    initial commit

--- app.R
+++ app.R
@@ -8,12 +8,22 @@ ui <- fluidPage(
             numericInput("pval", "Choose pval threshold", value = 0, min=0, max=1, step=0.05),
             numericInput("logFC", "Choose a logFC threshold", value = 1, min = 0, max = 4)
         ),
-        mainPanel()
+        mainPanel(
+            textOutput("pval_output"),
+            textOutput("logFC_output")
+        )
     )
 )
 
 server <- function(input, output) {
-
+    output$pval_output <- renderText({
+        paste0("pval threshold: ", input$pval)
+    })
+    
+    output$logFC_output <- renderText({
+        paste0("logFC theshold: ", input$logFC)
+        
+    })
 }
 
 shinyApp(ui, server)
