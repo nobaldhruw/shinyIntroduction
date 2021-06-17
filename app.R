70003f7bee4ca91fb750c95bf33026058442bc64
Nobal Dhruw <nobal.kishor@elucidata.io>
2021-06-17 09:52:49 +0530

    initial commit'

--- app.R
+++ app.R
@@ -5,24 +5,18 @@ ui <- fluidPage(
     
     sidebarLayout(
         sidebarPanel(
-            numericInput("pval", "Choose pval threshold", value = 0, min=0, max=1, step=0.05),
-            numericInput("logFC", "Choose a logFC threshold", value = 1, min = 0, max = 4)
+            sliderInput("num", "Enter a number", min=10, max = 100, value = 50, step = 10)
         ),
         mainPanel(
-            textOutput("pval_output"),
-            textOutput("logFC_output")
+            plotOutput("hist")
         )
     )
 )
 
 server <- function(input, output) {
-    output$pval_output <- renderText({
-        paste0("pval threshold: ", input$pval)
-    })
-    
-    output$logFC_output <- renderText({
-        paste0("logFC theshold: ", input$logFC)
-        
+    output$hist <- renderPlot({
+        x <- rnorm(input$num)
+        hist(x)
     })
 }
 
