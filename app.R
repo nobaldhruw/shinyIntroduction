ad979f84d7a79210f41403c330a22c02a34e996e
Nobal Dhruw <nobal.kishor@elucidata.io>
2021-06-17 08:31:59 +0530

    initial commit

--- app.R
+++ app.R
@@ -5,7 +5,16 @@ ui <- fluidPage(
     
     sidebarLayout(
         sidebarPanel("sidebar panel"),
-        mainPanel("main panel")
+        mainPanel(
+            h1("Heading 1"),
+            h2("Heading 2"),
+            h3("Heading 3"),
+            p("This is a simple paragraph"),
+            p("This is another paragraph"),
+            br(),
+            br(),
+            p("This is the last paragraph")
+        )
     )
 )
 
